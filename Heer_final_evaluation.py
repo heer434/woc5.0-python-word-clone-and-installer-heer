6d670a46b77ded14e0ab6031ce623ebf483b5b61
heer434 <122211091+heer434@users.noreply.github.com>
2023-08-06 15:48:25 +0530

    Update Heer_final_evaluation.py
    With Alignment

--- Heer_final_evaluation.py
+++ Heer_final_evaluation.py
@@ -4,29 +4,43 @@ from tkinter import font
 from tkinter import messagebox
 from tkinter import re
 
+
 root = Tk()
 
 
 # Change Font Size
 def font_size_chooser(e):
-	our_font.config(size=font_size_listbox.get(font_size_listbox.curselection()))
+    our_font.config(size=font_size_listbox.get(font_size_listbox.curselection()))
 
 # Change Font Style
 def font_style_chooser(e):
-	style = font_style_listbox.get(font_style_listbox.curselection()).lower()
+    style = font_style_listbox.get(font_style_listbox.curselection()).lower()
 
-	if style == "bold":
-		our_font.config(weight=style)
-	if style == "regular":
-		our_font.config(weight="normal", slant="roman", underline=0, overstrike=0)
-	if style == "italic":
-		our_font.config(slant=style)
-	if style == "underline":
-		our_font.config(underline=1)
-	
+    if style == "bold":
+        our_font.config(weight=style)
+    elif style == "regular":
+        our_font.config(weight="normal", slant="roman", underline=0, overstrike=0)
+    elif style == "italic":
+        our_font.config(slant=style)
+    elif style == "underline":
+        our_font.config(underline=1)
+
 # Create Font Chooser Function
 def font_chooser(e):
-	our_font.config(family=my_listbox.get(my_listbox.curselection()))
+    our_font.config(family=my_listbox.get(my_listbox.curselection()))
+
+# Text Alignment Functions
+def align_center():
+    my_text.tag_configure("center", justify="center")
+    my_text.tag_add("center", "1.0", "end")
+
+def align_left():
+    my_text.tag_configure("left", justify="left")
+    my_text.tag_add("left", "1.0", "end")
+
+def align_right():
+    my_text.tag_configure("right", justify="right")
+    my_text.tag_add("right", "1.0", "end")
 
 # Designate Our Font
 our_font = font.Font(family="Helvetica", size="32")
@@ -50,38 +64,38 @@ scrollbar.grid(row=0, column=1, sticky='ns')
 my_text.config(yscrollcommand=scrollbar.set)
 
 # Create Menu
-my_menu=Menu(root)
+my_menu = Menu(root)
 root.config(menu=my_menu)
 
 # New File Function
 def new_file():
-	my_text.delete("1.0",END)
-	root.title('Word Clone')
+    my_text.delete("1.0", END)
+    root.title('Word Clone')
 
 # Open File Function
 def open_file():
-	my_text.delete("1.0",END)
-	text_file = filedialog.askopenfilename(title="Open File",filetypes=(("TextFiles","*.txt"),("Python Files","*.py"),("PDF","*.pdf")))
-	text_file = open(text_file,'r')
-	stuff = text_file.read()
-	my_text.insert(END,stuff)
-	text_file.close()
+    my_text.delete("1.0", END)
+    text_file = filedialog.askopenfilename(title="Open File", filetypes=(("TextFiles", "*.txt"), ("Python Files", "*.py"), ("PDF", "*.pdf")))
+    text_file = open(text_file, 'r')
+    stuff = text_file.read()
+    my_text.insert(END, stuff)
+    text_file.close()
 
-# Save File Function	
+# Save File Function
 def save_file():
-	text_file = filedialog.asksaveasfilename(defaultextension=".*",title="Save File",filetypes=(("TextFiles",".txt"),("Python Files",".py"),("PDF",".pdf")))
-	text_file = open(text_file,'w')
-	text_file.write(my_text.get("1.0",END))
-	text_file.close()
+    text_file = filedialog.asksaveasfilename(defaultextension=".*", title="Save File", filetypes=(("TextFiles", ".txt"), ("Python Files", ".py"), ("PDF", ".pdf")))
+    text_file = open(text_file, 'w')
+    text_file.write(my_text.get("1.0", END))
+    text_file.close()
 
 # Add File Menu
-file_menu=Menu(my_menu, tearoff=False)
+file_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="New", command=new_file)
 file_menu.add_command(label="Open", command=open_file)
-file_menu.add_command(label="Save",command=save_file)
+file_menu.add_command(label="Save", command=save_file)
 file_menu.add_separator()
-file_menu.add_command(label="Exit",command=root.quit)
+file_menu.add_command(label="Exit", command=root.quit)
 
 # Bottom Frame
 bottom_frame = Frame(root)
@@ -102,26 +116,26 @@ my_listbox = Listbox(bottom_frame, selectmode=SINGLE, width=40)
 my_listbox.grid(row=1, column=0, padx=10)
 
 # Size Listbox
-font_size_listbox = Listbox(bottom_frame, selectmode=SINGLE, width=20 )
+font_size_listbox = Listbox(bottom_frame, selectmode=SINGLE, width=20)
 font_size_listbox.grid(row=1, column=1)
 
 # Style Listbox
-font_style_listbox = Listbox(bottom_frame, selectmode=SINGLE, width=20 )
+font_style_listbox = Listbox(bottom_frame, selectmode=SINGLE, width=20)
 font_style_listbox.grid(row=1, column=2, padx=10)
 
 # Add Font Families To Listbox
-for font in font.families():
-	my_listbox.insert('end', font)
+for font_name in font.families():
+    my_listbox.insert(END, font_name)
 
 # Add Sizes To Size Listbox
 font_sizes = [8, 10, 12, 14, 16, 20, 36, 48, 72]
 for size in font_sizes:
-	font_size_listbox.insert('end', size)
+    font_size_listbox.insert(END, size)
 
 # Add Styles To Style Listbox
 font_styles = ["Regular", "Bold", "Italic", "Underline"]
 for style in font_styles:
-	font_style_listbox.insert('end', style)	
+    font_style_listbox.insert(END, style)
 
 # Bind The Listbox
 my_listbox.bind('<ButtonRelease-1>', font_chooser)
@@ -145,16 +159,16 @@ def find_and_replace():
         # Set the text back to the widget
         my_text.delete("1.0", "end")
         my_text.insert("1.0", text)
-        
+
         # Clear the find and replace entries
         find_entry.delete(0, END)
         replace_entry.delete(0, END)
-	
+
     else:
         # Show an error message if the find word is not present
         messagebox.showerror("Error", "The find word is not present in the text")
 
-	
+
 # Add Find and Replace button
 find_button = Button(bottom_frame, text="Find and Replace", command=find_and_replace)
 find_button.grid(row=2, column=0, padx=10, pady=10)
@@ -165,5 +179,15 @@ find_entry.grid(row=2, column=1, padx=10, pady=10)
 
 replace_entry = Entry(bottom_frame)
 replace_entry.grid(row=2, column=2, padx=10, pady=10)
+
+# Add Alignment buttons
+align_center_button = Button(bottom_frame, text="Center", command=align_center)
+align_center_button.grid(row=3, column=0, padx=10, pady=10)
+
+align_left_button = Button(bottom_frame, text="Left", command=align_left)
+align_left_button.grid(row=3, column=1, padx=10, pady=10)
+
+align_right_button = Button(bottom_frame, text="Right", command=align_right)
+align_right_button.grid(row=3, column=2, padx=10, pady=10)
 
 root.mainloop()
